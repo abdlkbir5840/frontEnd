2046751f6d5164e650f0f18ec6f81764efab996c
abdlkbir5840 <habonbahmadi@gmail.com>
2023-12-11 14:51:59 +0100

    New version

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -7,10 +7,16 @@ export default function Main() {
           <h1 className="h2">Dashboard</h1>
           <div className="btn-toolbar mb-2 mb-md-0">
             <div className="btn-group me-2">
-              <button type="button" className="btn btn-sm btn-outline-secondary">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
                 Share
               </button>
-              <button type="button" className="btn btn-sm btn-outline-secondary">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
                 Export
               </button>
             </div>
@@ -27,35 +33,7 @@ export default function Main() {
         </div>
 
         <h2>Section title</h2>
-        <div className="table-responsive small">
-          <table className="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* <tr>
-                <td>1,001</td>
-                <td>random</td>
-                <td>data</td>
-                <td>placeholder</td>
-                <td>text</td>
-              </tr>
-              <tr>
-                <td>1,002</td>
-                <td>placeholder</td>
-                <td>irrelevant</td>
-                <td>visual</td>
-                <td>layout</td>
-              </tr> */}
-            </tbody>
-          </table>
-        </div>
+      {/* Table data here */}
       </main>
     </>
   );
