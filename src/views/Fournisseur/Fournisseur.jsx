f10076f5cc56b9efd872a1352cb29124360121cf
abdlkbir5840 <habonbahmadi@gmail.com>
2023-12-14 15:51:11 +0100

    new version

--- src/views/Fournisseur/Fournisseur.jsx
+++ src/views/Fournisseur/Fournisseur.jsx
@@ -3,27 +3,70 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import AddFournisseur from "./AddFournisseur";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchFournisseurs, removeFournisseur, selectFournisseurs } from "../../store/FournisseurSlice";
+import {
+  fetchFournisseurs,
+  removeFournisseur,
+  searchFournisseur,
+  selectFournisseurs,
+  totalPages,
+} from "../../store/FournisseurSlice";
 import EditFournisseur from "./EditFournisseur";
 export default function Clients() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const fournisseurs = useSelector(selectFournisseurs);
+  const totalPage = useSelector(totalPages);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
   useEffect(() => {
-    dispatch(fetchFournisseurs())
+    dispatch(fetchFournisseurs(1));
   }, [dispatch]);
 
-  const handleDeleteFournisseur =  (fournisseur) => {
-    console.log(fournisseur);
-     dispatch(removeFournisseur(fournisseur));
-  
-  };  
-  const handleEditFournisseur = (fournisseur) => {
-    console.log(fournisseur);
+  const handleDeleteFournisseur = (fournisseur) => {
+    dispatch(removeFournisseur(fournisseur));
+  };
+  const handelPaginate = (page) => {
+    //ajouter condition if search != "" dispatch searchFournisseur()
+    if(currentPage>=totalPage){ 
+      setCurrentPage(1);
+    dispatch(fetchFournisseurs(1));
+    }else{
+    dispatch(fetchFournisseurs(page));
+    setCurrentPage(page);
+  }
+
+  };
+
+  const renderPaginationLinks = () => {
+    if (totalPage < 5)
+      return Array.from({ length: totalPage }, (_, index) => index + 1);
+    const maxLinksToShow = 5;
+    const halfMaxLinksToShow = Math.floor(maxLinksToShow / 2);
+
+    let startIdx = currentPage - halfMaxLinksToShow;
+    let endIdx = currentPage + halfMaxLinksToShow;
+
+    if (startIdx < 1) {
+      startIdx = 1;
+      endIdx = maxLinksToShow;
+    }
+
+    if (endIdx > totalPage) {
+      endIdx = totalPage;
+      startIdx = totalPage - maxLinksToShow + 1;
+    }
+
+    return Array.from(
+      { length: endIdx - startIdx + 1 },
+      (_, index) => startIdx + index
+    );
+  };
+  const handleSearch = () => {
+    dispatch(searchFournisseur(search));
   };
   return (
     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 className="h2">Dashboard</h1>
+        <h1 className="h2">Fournisseur Data</h1>
         <div className="btn-toolbar mb-2 mb-md-0">
           <div className="btn-group me-2">
             <button type="button" className="btn btn-sm btn-outline-secondary">
@@ -33,11 +76,28 @@ export default function Clients() {
               Export
             </button>
           </div>
-          <AddFournisseur />
         </div>
       </div>
-
-      <h2>Fournisseur Data</h2>
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <AddFournisseur />
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            onClick={handleSearch}
+            className="btn btn-outline-primary"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
       <div className="table-responsive small">
         <table className="table">
           <thead>
@@ -48,6 +108,7 @@ export default function Clients() {
               <th scope="col">Téléphone</th>
               <th scope="col">Email</th>
               <th scope="col">Fax</th>
+              <th scope="col"></th>
               <th scope="col"></th>
             </tr>
           </thead>
@@ -61,12 +122,6 @@ export default function Clients() {
                   <td>{fournisseur.tel}</td>
                   <td>{fournisseur.mail}</td>
                   <td>{fournisseur.fax}</td>
-                  {/*<td>*/}
-                  {/*    <button onClick={()=> handleCheckProduct(product)} className={"btn btn-outline-success"}>*/}
-                  {/*        <FontAwesomeIcon icon={product.checked ? faCheckCircle:faCircle}>*/}
-                  {/*        </FontAwesomeIcon>*/}
-                  {/*    </button>*/}
-                  {/*</td>*/}
                   <td>
                     <button
                       onClick={() => handleDeleteFournisseur(fournisseur)}
@@ -76,20 +131,63 @@ export default function Clients() {
                     </button>
                   </td>
                   <td>
-                    <button
-                      onClick={() => handleEditFournisseur(fournisseur)}
-                      className={"btn btn-outline-secondary"}
-                    >
-                      <FontAwesomeIcon icon={faEdit}>
-                        <EditFournisseur />
-                      </FontAwesomeIcon>
-                      
-                    </button>
+                    <EditFournisseur
+                      fournisseurInfo={{
+                        id: fournisseur.id,
+                        nom: fournisseur.nom,
+                        code_fournisseur: fournisseur.code_fournisseur,
+                        tel: fournisseur.tel,
+                        fax: fournisseur.fax,
+                        mail: fournisseur.mail,
+                        adresse: fournisseur.adresse,
+                      }}
+                    />
                   </td>
                 </tr>
               ))}
           </tbody>
         </table>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li
+              class="page-item"
+              onClick={() => handelPaginate(currentPage - 1)}
+            >
+              <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            {renderPaginationLinks().map((page) => (
+              <li
+                onClick={() => handelPaginate(page)}
+                key={page}
+                className={
+                  page === currentPage ? "page-item active" : "page-item"
+                }
+              >
+                <a href="#" className="page-link">
+                  {page}
+                </a>
+              </li>
+            ))}
+            <li
+              class={
+                currentPage === totalPage ? "page-item disabled" : "page-item"
+              }
+              onClick={() => handelPaginate(currentPage + 1)}
+            >
+              <a
+                class={
+                  currentPage === totalPage ? "page-link disabled" : "page-link"
+                }
+                href="#"
+                aria-label="Next"
+              >
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </main>
   );
