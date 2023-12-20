711cafb2aadc233734b5088773166198e7d3178d
unknown <ysfmh2002@gmail.com>
2023-12-20 16:50:06 +0100

    new version

--- src/apis/AxiosInstance.js
+++ src/apis/AxiosInstance.js
@@ -1,7 +1,31 @@
 import axios from "axios";
-import Cookie from 'cookie-universal'
-const cookies = Cookie()
+import Cookie from 'cookie-universal';
+
 export const instance = axios.create({
     baseURL: 'http://localhost:8000/api/',
-    headers: {'Authorization': 'Bearer '+cookies.get('login_token')}
-  });
+});
+
+// Ajout d'un intercepteur pour les requêtes sortantes
+instance.interceptors.request.use(
+    async (config) => {
+        // Récupération du token de manière asynchrone
+        const token = await getToken();
+
+        // Ajout du token aux en-têtes de la requête
+        config.headers['Authorization'] = `Bearer ${token}`;
+
+        return config;
+    },
+    (error) => {
+        // Gestion des erreurs de requête
+        return Promise.reject(error);
+    }
+);
+
+// Fonction pour obtenir le token à partir des cookies
+async function getToken() {
+    const cookies = Cookie();
+    const loginToken = cookies.get('login_token');
+    return loginToken || '';
+}
+
